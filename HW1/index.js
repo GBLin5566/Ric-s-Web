f382cde1a5311813056c58f701555c14c854b65d
RicLab <riclab@RicLabdeMacBook-Air.local>
2015-09-26 23:41:11 +0800

    features is done, still need to improve the UI

--- HW1/index.js
+++ HW1/index.js
@@ -1,14 +1,32 @@
 
+var remainTodo = 0;
 function handleEnter (e) {
     var text = document.getElementById("enterInput");
-    if (e.keyCode === 13 ) {
-        var form = document.getElementsByTagName("Form")[0];
-        var todo = document.createTextNode("ToDo List");
-        form.appendChild(document.createElement("h1").appendChild(todo));
-        var newLi = document.createElement("li").appendChild(document.createTextNode(text.value));
-        console.log(newLi);
+    if (e.keyCode === 13 && text.value != "") {
+        var newLi = document.createElement("li");
+        newLi.innerHTML = text.value;
+        newLi.className = 'new-task';
+        newLi.type = 'circle';
+        newLi.addEventListener("mousedown", function(){handleMouseDownTasks(newLi)},false);
+        document.getElementsByTagName("body")[0].appendChild(newLi);
         text.value = "";
+        refreshTodo(1);
         return false;
     }
+    else if (e.keyCode === 13 && text.value == "")
+        return false;
     return true;
+}
+
+function handleMouseDownTasks (li) {
+    if(li.className != 'remove-task'){
+        li.className = 'remove-task';
+        refreshTodo(-1);
+    }
+}
+
+function refreshTodo (num){
+    remainTodo += num;
+    console.log(remainTodo);
+    document.getElementById('remainTasks').innerHTML = remainTodo;
 }
