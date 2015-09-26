e5a5347e4a7ef21cdd544415f43cd9e041b69fba
RicLab <riclab@RicLabdeMacBook-Air.local>
2015-09-26 22:16:26 +0800

    Can input the text, still need to fix the to do list bug

--- HW1/index.js
+++ HW1/index.js
@@ -1,4 +1,14 @@
 
-var text = document.getElementById("enterInput");
-
-console.log(text);
+function handleEnter (e) {
+    var text = document.getElementById("enterInput");
+    if (e.keyCode === 13 ) {
+        var form = document.getElementsByTagName("Form")[0];
+        var todo = document.createTextNode("ToDo List");
+        form.appendChild(document.createElement("h1").appendChild(todo));
+        var newLi = document.createElement("li").appendChild(document.createTextNode(text.value));
+        console.log(newLi);
+        text.value = "";
+        return false;
+    }
+    return true;
+}
