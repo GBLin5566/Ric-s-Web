a5cfdf1565bbb951910f91467b3021fe586b9ca1
GBLin <r04943092@ntu.edu.tw>
2015-10-01 12:14:24 +0800

    new close button img

--- HW1/index.js
+++ HW1/index.js
@@ -21,15 +21,22 @@ function handleEnter (e) {
     return true;
 }
 
+
 function handleMouseDownTasks (li) {
     if(li.className != 'remove-task'){
         li.className = 'remove-task';
         refreshTodo(-1);
-        var btn = document.createElement("button");
+        /*var btn = document.createElement("button");
         btn.className = 'remove-btn';
         btn.addEventListener("mousedown", function(){removeTasks(li)}, false);
         btn.appendChild(document.createTextNode("X"));
-        li.appendChild(btn);
+        li.appendChild(btn);*/
+        var inp = document.createElement("input");
+        inp.type = "image";
+        inp.src = "thexx.png";
+        inp.id = "myImage";
+        inp.addEventListener("mousedown", function(){removeTasks(li)}, false);
+        li.appendChild(inp);
     }
 }
 
