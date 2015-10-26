4c2384c85d903a6c72164b4fb23e092792d073e1
GBLin <r04943092@ntu.edu.tw>
2015-10-26 23:26:23 +0800

    Server works, but web failed somehow

--- HW4/devServer.js
+++ HW4/devServer.js
@@ -18,6 +18,10 @@ app.get('*', function(req, res) {
 });
 
 app.listen(3000, 'localhost', function(err) {
-  if (err) { return console.log(err); }
+  if (err) {
+    console.log(err);
+    return;
+  }
+
   console.log('Listening at http://localhost:3000');
 });
